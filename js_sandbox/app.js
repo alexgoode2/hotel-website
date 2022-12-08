77ffeb8d9ab758c17324f2aef8c324dd5d39f62d
Alex Goode <alexgoode@alexs-mbp-3.mynetworksettings.com>
2022-12-08 01:02:02 -0500

    first draft

---
+++ js_sandbox/app.js
@@ -0,0 +1,12 @@
+//var,let,const
+
+var name = 'John Doe';
+console.log(name);
+name='steve smith'
+console.log(name);
+
+//Init var
+var greeting;
+console.log(greeting);
+greeting = 'hello'
+console.log(greeting);
